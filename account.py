c448d0913e58aae69f388476758a0f9839d27c21
Keshava-369 <keshavaharshith30@gmail.com>
2026-02-28 17:06:42 +0530

    Simple Changes

--- account.py
+++ account.py
@@ -1,22 +1,27 @@
-class Account:
-    def __init__(self, person,balance=0):
-        self.balance = balance
-        self.person = person
+class Account: 
+    acc_count = 0
+    def __init__(self,Person,acc_type='Saving Account'):
+        self.Person = Person
+        self.acc_type = acc_type
+        self.balance = 0
+        self.transaction = []
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            return True  # Deposit Done
-        return False  # Deposit fail
+    def deposit(self,amount): #Deposit Amount
+        self.balance += amount
+        print(f'The Balance After Depositing:{self.balance}')
+        self.transaction.append(amount)
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            return True  # Withdrawal Done
-        return False  # Withdrawal fail
+    def withdraw(self,amount): #Withdraw Amount
+        print(f'The Amount You Entered to Withdraw is:{amount}')
+        self.balance -= amount
+        print(f'The Amount Withdrawn Successfully \n The Balance:{self.balance}')
 
-    def display_balance(self): #Display Balance
-        print(f"Current balance: ${self.get_balance()}")
-        
-    def disp_info(self):
-        print(f'The Holder, Balance{self.person,self.balance}')
+    def check_balnce(self): #Balance Enquiry
+        print(f'The Leftover Balance in Your Account is:{self.balance}')
+
+    def account_info(self): #Account Basic Details
+        print(f'The Account Holder Name:{self.Person}')
+        print(f'The Account Holder Account Type:{self.acc_type}')
+
+    def transaction(self): #Total Transactions
+        print(f'The Transactions are:{self.transaction}')
